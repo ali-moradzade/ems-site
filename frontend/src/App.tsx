4d3e6d488908f07e274be3a4320e1b4c5b3a1194
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-02 18:12:30 +0330

    adding welcome panel to login&signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {LoginPage} from "./pages/LoginPage";
 import {SignupPage} from "./pages/SignupPage";
 
 export function App() {
     return (
         <div className="container-fluid">
-            <LoginPage/>
             <SignupPage/>
         </div>
     );
---
+++ frontend/src/components/WelcomePanel.tsx
@@ -0,0 +1,9 @@
+export function WelcomePanel() {
+    return (
+        <div className="row justify-content-center align-items-center bg-success text-light" style={{height: '80px'}}>
+            <div className="col text-center">
+                <h1>Welcome to ESM Website</h1>
+            </div>
+        </div>
+    );
+}
