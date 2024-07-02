6aae72832815555334433feac8ec51b32206921f
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-02 17:54:26 +0330

    updating loginPage: login using email&password

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -10,12 +10,12 @@ export function LoginPage() {
                                 <p className="small text-muted">Login with your username &amp; password</p>
                                 <form>
                                     <div className="mb-3">
-                                        <input placeholder="Username" type="text" id="username"
+                                        <input placeholder="Email" type="email" id="loginEmail"
                                                className="form-control form-control mt-2"
                                                required/>
                                     </div>
                                     <div className="mb-3">
-                                        <input placeholder="Password" type="password" id="password"
+                                        <input placeholder="Password" type="password" id="loginPassword"
                                                className="form-control form-control mt-2"
                                                required/>
                                     </div>
