f93bf01198f5b182e57802fe86b0ef54e54a1765
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-03 13:22:41 +0330

    adding basic jobs module, service and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import {Module} from '@nestjs/common';
 import { EmployeesModule } from './employees/employees.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-    imports: [EmployeesModule],
+    imports: [EmployeesModule, JobsModule],
     controllers: [],
     providers: [],
 })
