467662f5096e7deb18846efa7956df8a493e1c3a
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-03 16:04:27 +0330

    adding employee service methods

--- backend/src/employees/employees.service.ts
+++ backend/src/employees/employees.service.ts
@@ -1,5 +1,62 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Employee} from "./employee.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class EmployeesService {
+    constructor(
+        @InjectRepository(Employee) private repo: Repository<Employee>,
+    ) {
+    }
+
+    create(
+        email: string, password: string, firstName: string, lastName: string,
+        phone: string, job: string, date: string
+    ) {
+        const employee = this.repo.create({
+            email, password, firstName,
+            lastName, phone, job, date,
+        });
+
+        return this.repo.save(employee);
+    }
+
+    findOne(id: number) {
+        if (!id) {
+            return null;
+        }
+
+        return this.repo.findOneBy({
+            id,
+        });
+    }
+
+    find(email: string) {
+        return this.repo.findBy({
+            email,
+        });
+    }
+
+    async update(id: number, attrs: Partial<Employee>) {
+        const employee = await this.findOne(id);
+
+        if (!employee) {
+            throw new NotFoundException('Employee not found');
+        }
+
+        Object.assign(employee, attrs);
+
+        return this.repo.save(employee);
+    }
+
+    async remove(id: number) {
+        const employee = await this.findOne(id);
+
+        if (!employee) {
+            throw new NotFoundException('Employee not found');
+        }
+
+        return this.repo.remove(employee);
+    }
 }
