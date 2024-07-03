88254d5e9cdcd8573e79e098e5b3a63d0ebff971
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-03 16:09:10 +0330

    adding jobs service methods

--- backend/src/jobs/jobs.service.ts
+++ backend/src/jobs/jobs.service.ts
@@ -1,5 +1,51 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Job} from "./jobs.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class JobsService {
+    constructor(
+        @InjectRepository(Job) private repo: Repository<Job>,
+    ) {
+    }
+
+    create(name: string, date: string) {
+        const job = this.repo.create({name, date});
+        return this.repo.save(job);
+    }
+
+    findOne(id: number) {
+        if (!id) {
+            return null;
+        }
+
+        return this.repo.findOneBy({id});
+    }
+
+    find(name: string) {
+        return this.repo.findBy({name});
+    }
+
+    async update(id: number, attrs: Partial<Job>) {
+        const job = await this.findOne(id);
+
+        if (!job) {
+            throw new NotFoundException('Job not found');
+        }
+
+        Object.assign(job, attrs);
+
+        return this.repo.save(job);
+    }
+
+    async remove(id: number) {
+        const job = await this.findOne(id);
+
+        if (!job) {
+            throw new NotFoundException('Job not found');
+        }
+
+        return this.repo.remove(job);
+    }
 }
