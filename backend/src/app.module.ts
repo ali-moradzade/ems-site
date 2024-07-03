b21227e46f1404154dd3cdf86ab09cee4752a403
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-03 13:21:47 +0330

    adding basic employees module, service and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [EmployeesModule],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {
 }
