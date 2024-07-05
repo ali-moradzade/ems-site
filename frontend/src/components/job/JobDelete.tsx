6677204f5df424e93c6ec427372a1ee84e4419ea
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-05 11:30:27 +0330

    adding JobDelete comp

---
+++ frontend/src/components/job/JobDelete.tsx
@@ -0,0 +1,50 @@
+import {Job} from "../../apis/jobs";
+import {useJobContext} from "../../hooks/use-job-context";
+import {FormEvent, useRef} from "react";
+
+interface JobDeleteProps {
+    job: Job;
+}
+
+export function JobDelete({job}: JobDeleteProps) {
+    const {id, name} = job;
+    const {deleteJob} = useJobContext();
+
+    const closeRef = useRef<HTMLButtonElement>(null);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        deleteJob(id).then();
+
+        closeRef.current?.click();
+    };
+
+    return (
+        <div className="modal fade" id={`delete_job_${id}`} tabIndex={-1}
+             aria-labelledby={`delete_job_${id}`} aria-hidden="true"
+        >
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5">Delete Job Confirmation</h1>
+                        <button
+                            type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                            ref={closeRef}
+                        ></button>
+                    </div>
+                    <div className="modal-body">
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <p>Are you sure you want to delete <span className="fw-bold">"{name}"</span>? </p>
+                            </div>
+                            <div className="mb-3 float-end">
+                                <button className="btn btn-danger btn-sm">Delete</button>
+                                <button className="btn btn-secondary btn-sm ms-2">Cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
