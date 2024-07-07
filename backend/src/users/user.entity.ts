bb21c10b7cf96c16b7cc185b0be4e068ab7d4d84
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-07 14:27:47 +0330

    adding user entity

---
+++ backend/src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import {Column, PrimaryGeneratedColumn} from "typeorm";
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+}
