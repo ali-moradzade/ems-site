1e838db2f95b19aab8f3ff14e6ff403980e8baac
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-07 14:22:55 +0330

    adding basic users module, service and controller files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import {EmployeesModule} from './employees/employees.module';
 import {JobsModule} from './jobs/jobs.module';
 import {Employee} from "./employees/employee.entity";
 import {Job} from "./jobs/jobs.entity";
+import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [
         EmployeesModule,
         JobsModule,
+        UsersModule,
         TypeOrmModule.forRoot({
             type: 'sqlite',
             database: 'db.sqlite',
