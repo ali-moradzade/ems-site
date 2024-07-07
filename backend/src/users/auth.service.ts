490b0176a22d366eee2b38165c02025710c02bcd
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-07 17:45:35 +0330

    adding auth service

--- backend/src/users/auth.service.ts
+++ backend/src/users/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable, NotFoundException} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {randomBytes, scrypt as _scrypt} from 'crypto';
+import {promisify} from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+    ) {
+    }
+
+    async signup(email: string, password: string, firstName: string, lastName: string) {
+        const users = await this.usersService.find(email);
+
+        if (users.length) {
+            throw new BadRequestException('Email in use');
+        }
+
+        const salt = randomBytes(8).toString('hex');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        const result = `${salt}.${hash.toString('hex')}`;
+
+        return await this.usersService.create(email, result, firstName, lastName);
+    }
+
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        const [salt, storedHash] = user.password.split('.');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if (storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('Invalid credentials');
+        }
+
+        return user;
+    }
+}
