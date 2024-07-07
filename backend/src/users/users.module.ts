77c8c7cadafb03a8c049c847b0e2f979b1b7db86
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-07 18:03:37 +0330

    adding current user interceptor and decorator

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import {Module} from '@nestjs/common';
+import {UsersService} from './users.service';
+import {UsersController} from './users.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./user.entity";
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
+import {CurrentUserInterceptor} from "./interceptors/current-user.interceptor";
+import {APP_INTERCEPTOR} from "@nestjs/core";
 
 @Module({
-  providers: [UsersService, AuthService],
-  controllers: [UsersController],
-  imports: [
-      TypeOrmModule.forFeature([
-          User,
-      ])
-  ]
+    providers: [
+        UsersService,
+        AuthService,
+        {
+            provide: APP_INTERCEPTOR,
+            useValue: CurrentUserInterceptor,
+        }
+    ],
+    controllers: [UsersController],
+    imports: [
+        TypeOrmModule.forFeature([
+            User,
+        ])
+    ]
 })
-export class UsersModule {}
+export class UsersModule {
+}
