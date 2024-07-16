e688d22ab78ef27a8e9004d1008ef052358aa1af
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-16 23:33:11 +0330

    adding tests for users service

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,18 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import {beforeEach, describe, expect, it} from "vitest";
+import {Test, TestingModule} from '@nestjs/testing';
+import {UsersController} from './users.controller';
+import {AuthService} from "./auth.service";
+import {UsersService} from "./users.service";
 
 describe('UsersController', () => {
-  let controller: UsersController;
+    let controller: UsersController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [UsersController],
+            providers: [
+                {
+                    provide: UsersService,
+                    useValue: {}
+                },
+                {
+                    provide: AuthService,
+                    useValue: {}
+                },
+            ]
+        }).compile();
 
-    controller = module.get<UsersController>(UsersController);
-  });
+        controller = module.get<UsersController>(UsersController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,18 +1,67 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import {beforeEach, describe, expect, test} from 'vitest';
+import {Test, TestingModule} from '@nestjs/testing';
+import {UsersService} from './users.service';
+import {Repository} from "typeorm";
+import {User} from "./user.entity";
+import {getRepositoryToken} from "@nestjs/typeorm";
+import {AppModule} from "../app.module";
+import {BadRequestException} from '@nestjs/common';
 
 describe('UsersService', () => {
-  let service: UsersService;
+    let service: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    const mockUser = {
+        email: 'mock@mock.com',
+        password: 'mockedPassword239723',
+        firstName: 'mockFirstName',
+        lastName: 'mockLastName',
+    };
 
-    service = module.get<UsersService>(UsersService);
-  });
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                AppModule,
+            ],
+            providers: [
+                UsersService,
+                {
+                    provide: getRepositoryToken(User),
+                    useClass: Repository,
+                }
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<UsersService>(UsersService);
+    });
+
+    test('creates users service', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('create', () => {
+        test('given user properties, creates user', async () => {
+            const {email, password, firstName, lastName} = mockUser;
+
+            const user = await service.create(email, password, firstName, lastName);
+
+            expect(user).toBeDefined();
+            expect(user.email).toEqual(email);
+        });
+
+        test('creating user with duplicate email, throws error', async () => {
+            const {email, password, firstName, lastName} = mockUser;
+
+            await service.create(email, password, firstName, lastName);
+
+            await expect(service.create(email, password, firstName, lastName)).rejects.toThrow(BadRequestException);
+        });
+    });
+
+    describe('findOne', () => {
+        test('no users, returns null', async () => {
+            const user = await service.findOne(1);
+
+            expect(user).toBeNull()
+        });
+    });
 });
