4c069d59ba1810779c089c9b2b39aff040f0412e
Ali Moradzade <alimorizz1379@gmail.com>
2024-07-16 22:47:23 +0330

    adding unit tests for auth service: signup&login

--- backend/src/users/auth.service.spec.ts
+++ backend/src/users/auth.service.spec.ts
@@ -1,18 +1,101 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import {beforeEach, describe, expect, test} from "vitest";
+import {Test, TestingModule} from '@nestjs/testing';
+import {AuthService} from './auth.service';
+import {UsersService} from "./users.service";
+import {User} from "./user.entity";
+import {BadRequestException, NotFoundException} from "@nestjs/common";
 
 describe('AuthService', () => {
-  let service: AuthService;
+    let service: AuthService;
+    let fakeUsersService: Partial<UsersService>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    const mockUser = {
+        email: 'mock@mock.com',
+        password: 'mockedPassword239723',
+        firstName: 'mockFirstName',
+        lastName: 'mockLastName',
+    };
 
-    service = module.get<AuthService>(AuthService);
-  });
+    beforeEach(async () => {
+        const users: User[] = [];
+        fakeUsersService = {
+            find(email: string): Promise<User[]> {
+                const filteredUsers = users.filter(user => user.email === email);
+                return Promise.resolve(filteredUsers);
+            },
+            async create(email: string, password: string, firstName: string, lastName: string): Promise<User> {
+                const user = {
+                    id: Math.floor(Math.random() * 99999),
+                    email, password, firstName, lastName,
+                };
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+                users.push(user);
+                return Promise.resolve(user);
+            }
+        };
+
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUsersService,
+                }
+            ],
+        }).compile();
+
+        service = module.get<AuthService>(AuthService);
+    });
+
+    test('creates an instance of service', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('signup', () => {
+        test('given user properties, creates user with salted and hashed password', async () => {
+            const {email, password, firstName, lastName} = mockUser;
+
+            const user = await service.signup(email, password, firstName, lastName);
+            const [salt, hash] = user.password.split('.');
+
+            expect(user).toBeDefined();
+            expect(user.password).not.toEqual(password);
+            expect(salt).toBeDefined();
+            expect(hash).toBeDefined();
+        });
+
+        test('duplicate email, throws error: BadRequestException', async () => {
+            const {email, password, firstName, lastName} = mockUser;
+
+            await service.signup(email, password, firstName, lastName);
+
+            await expect(service.signup(email, password, firstName, lastName)).rejects.toThrow(BadRequestException);
+        });
+    });
+
+    describe('login', () => {
+        test('given user credentials, logins the user', async () => {
+            const {email, password, firstName, lastName} = mockUser;
+
+            await service.signup(email, password, firstName, lastName);
+            const user = await service.login(email, password);
+
+            expect(user).toBeDefined();
+            expect(user.email).toEqual(email);
+        });
+
+        test('user not signed up, throws error: NotFoundException', async () => {
+            const {email, password} = mockUser;
+
+            await expect(service.login(email, password)).rejects.toThrow(NotFoundException);
+        });
+
+        test('invalid credentials, throws error: BadRequestException', async () => {
+            const {email, password, firstName, lastName} = mockUser;
+
+            await service.signup(email, password, firstName, lastName);
+
+            await expect(service.login(email, 'some invalid password')).rejects.toThrow(BadRequestException);
+        });
+    });
 });
