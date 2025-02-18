5345280b77931b71386382f729d35bdcfee9b423
Ali Moradzade <alimorizz1379@gmail.com>
2025-02-18 23:21:50 +0330

    adding PORT to env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,18 +4,24 @@ import {SwaggerModule} from '@nestjs/swagger';
 import * as fs from 'fs';
 import * as yaml from 'yaml';
 import {join} from "path";
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
     app.enableCors();
-    app.setGlobalPrefix('api')
+    app.setGlobalPrefix('api');
 
     // Load the custom OpenAPI spec file, and serve it using Swagger
     const file = fs.readFileSync(join(__dirname, '..', '..', 'docs', 'open-api.yaml'), 'utf8');
     const openAPISpec = yaml.parse(file);
     SwaggerModule.setup('docs', app, openAPISpec);
 
-    await app.listen(3300);
+    const PORT = configService.get<number>('PORT', 3300);
+    await app.listen(PORT);
+
+    console.log(`Application is running on: http://localhost:${PORT}`);
 }
 
 bootstrap().then();
